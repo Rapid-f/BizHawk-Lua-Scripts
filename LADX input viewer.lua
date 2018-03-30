5305cc4bda6c74b339f9185bacabc52665fb8af8
Rapid-f <37719096+Rapid-f@users.noreply.github.com>
2018-03-29 23:09:07 -0600

    Add files via upload

--- LADX input viewer.lua
+++ LADX input viewer.lua
@@ -1,7 +1,7 @@
 function drawController(x, y, inputData)
 	local buttonID   	   = {inputData.A, inputData.B, inputData.Down, inputData.Left, inputData.Right, inputData.Select, inputData.Start, inputData.Up}
 	local pressedbuttonPos = {bX = {9, 8, 2, 1, 3, 5, 6, 2},
-							  bY = {2, 2, 3, 2, 2, 3, 3, 1}}
+	                          bY = {2, 2, 3, 2, 2, 3, 3, 1}}
 	
 	gui.drawRectangle(x, y, 10, 4, 0x8F0061FF, 0x8F0061FF)
 	gui.drawLine(x+1, y+2, x+3, y+2, 0xFFF000000)
@@ -18,8 +18,8 @@ while true do
 	GAMEPLAY_WORLD = 0x0B
 	local currentInputs  = joypad.get()
 	local linkSpritePos  = {x              = memory.read_u8(0xFF98),
-							y              = memory.read_u8(0xC145),
-							y_puddleOffset = memory.read_u8(0xC13B)}
+	                        y              = memory.read_u8(0xC145),
+	                        y_puddleOffset = memory.read_u8(0xC13B)}
 	
 	if linkSpritePos.x < 6   then linkSpritePos.x = 6 elseif linkSpritePos.x > 155 then linkSpritePos.x = 155 end
 	if linkSpritePos.y > 131 then linkSpritePos.y = 0 end
